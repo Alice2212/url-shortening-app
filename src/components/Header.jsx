c07091567c87ba8aa1ca9f34e74e80390d582dc6
Alice2212 <hezealice@gmail.com>
2023-01-02 16:38:57 +0100

    header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Logo from "../assets/images/logo.svg"
+
+const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <div>
+          {/* logo */}
+          <img src={Logo} alt="logo" />
+
+          {/* nav-links */}
+          <nav>
+            <ul>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Resourses</li>
+            </ul>
+          </nav>
+        </div>
+
+        <div>
+          <ul>
+            <li>Login</li>
+            <li className='btn-signUp'>Sign Up</li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header
