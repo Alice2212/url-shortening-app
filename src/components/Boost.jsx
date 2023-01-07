6d3869c0b0c3b1d17950288bd3dcfc0bdb1ed050
Alice2212 <hezealice@gmail.com>
2023-01-07 13:47:38 +0100

    boost component

--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -5,13 +5,15 @@ import bgMobile from "../assets/images/bg-boost-mobile.svg"
 const Boost = () => {
   return (
     <>
-      <div className="boost text-slate-100">
+      <div className="boost text-slate-100 relative">
         <picture>
-          <source media="(min-width: 760px)" srcSet={bgDesktop}/>
+          <source media="(min-width: 760px)" srcSet={bgDesktop} />
           <img src={bgMobile} alt="" />
         </picture>
-        <h2 className="mb-4">Boost your links today</h2>
-        <button className="btn-cta">Get Started</button>
+        <div className="flex items-center justify-center flex-col boost-inner">
+          <h2 className="mb-4">Boost your links today</h2>
+          <button className="btn-cta">Get Started</button>
+        </div>
       </div>
     </>
   );
