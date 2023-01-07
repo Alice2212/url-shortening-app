eee03017a7030dbee5a5fe8db604339099472807
Alice2212 <hezealice@gmail.com>
2023-01-07 13:25:06 +0100

    boost component

--- src/components/Boost.jsx
+++ src/components/Boost.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-// import bgImg from "../assets/images/bg-boost-desktop.svg"
+import bgDesktop from "../assets/images/bg-boost-desktop.svg"
+import bgMobile from "../assets/images/bg-boost-mobile.svg"
 
 const Boost = () => {
   return (
     <>
-      <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-32 text-center flex items-center justify-center flex-col text-slate-100">
+      <div className="boost text-slate-100">
+        <picture>
+          <source media="(min-width: 760px)" srcSet={bgDesktop}/>
+          <img src={bgMobile} alt="" />
+        </picture>
         <h2 className="mb-4">Boost your links today</h2>
         <button className="btn-cta">Get Started</button>
       </div>
