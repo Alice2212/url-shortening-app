d24c5514a1b4a07a36e187e252326d92142fd1de
Alice2212 <hezealice@gmail.com>
2023-01-10 23:29:03 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,8 @@ const Hero = () => {
     <>
       <div>
         <h1>MORE THAN JUST SHORTHER LINKS</h1>
+        <p>Build your brand's recognition and get detailed insights on how your links are performing</p>
+        <button className='btn-cta'></button>
       </div>
     </>
   )
