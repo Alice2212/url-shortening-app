baf8665c7f7704ba5ee0a1dc314b0ab891888a5f
Alice2212 <hezealice@gmail.com>
2023-01-10 23:30:52 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <>
-      <div>
-        <h1>MORE THAN JUST SHORTHER LINKS</h1>
-        <p>Build your brand's recognition and get detailed insights on how your links are performing</p>
-        <button className='btn-cta'></button>
-      </div>
+      <section>
+        <div>
+          <h1>MORE THAN JUST SHORTHER LINKS</h1>
+          <p>
+            Build your brand's recognition and get detailed insights on how your
+            links are performing
+          </p>
+          <button className="btn-cta">Get Started</button>
+        </div>
+        <div>
+          
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
