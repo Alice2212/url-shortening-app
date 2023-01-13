776260952cfcecf7e235820af295b09a9783be17
Alice2212 <hezealice@gmail.com>
2023-01-13 14:42:06 +0100

    advance page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Boost from "./components/Boost";
 import Hero from "./components/Hero";
+import Advance from "./components/Advance";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Advance />
       <Boost />
       <Footer />
     </>
---
+++ src/components/Advance.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Advance = () => {
+  return (
+    <>
+      <section className='h-96 bg-slate-200'>
+        <div className='max-width'>
+            {/* advance title */}
+            <div className='text-center'>
+                <h2>Advance Statistics</h2>
+            </div>
+            {/* advance cards */}
+            <div>
+
+            </div>
+
+        </div>
+
+      </section>
+    </>
+  )
+}
+
+export default Advance
