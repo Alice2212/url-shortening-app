591063d2153659b5d0341c35c511ec3f57ebfd18
Alice2212 <hezealice@gmail.com>
2023-01-13 14:28:23 +0100

    hero responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,17 +4,17 @@ import heroImg from "../assets/images/illustration-working.svg";
 const Hero = () => {
   return (
     <>
-      <section className="max-width flex lg:flex-nowrap md:flex-wrap flex-row my-12">
-        <div className="">
-          <h1 className="text-3xl text-bold font-black leading-12">More than just <br></br>Shoerter links</h1>
-          <p>
+      <section className="max-width flex lg:flex-nowrap flex-wrap flex-row my-12 mx-auto items-center  justify-between text-center lg:text-left">
+        <div className="order-2 lg:order-1 mr-4 lg:my-0 mt-8">
+          <h1 className="text-4xl text-bold font-black leading-[1.25] tracking-wider">More than just <br></br>Shorter links</h1>
+          <p className="text-gray-500 py-4"> 
             Build your brand's recognition and get detailed insights on how your
             links are performing
           </p>
           <button className="btn-cta">Get Started</button>
         </div>
-        <div className="">
-          <img src={heroImg} alt="hero iamge"/>
+        <div className="order-1 lg:order-2 ">
+          <img src={heroImg} alt="hero iamge " />
         </div>
       </section>
     </>
