bf634acbc9701b179cead36e66083272ed51beff
Alice2212 <hezealice@gmail.com>
2023-01-13 15:29:43 +0100

    advance page

--- src/components/Advance.jsx
+++ src/components/Advance.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 const Advance = () => {
   return (
     <>
-      <section className='h-96 bg-slate-200'>
-        <div className='max-width'>
+      <section className='advance-wrapper bg-slate-200'>
+        <div className='max-width py-16'>
             {/* advance title */}
             <div className='text-center'>
-                <h2>Advance Statistics</h2>
+                <h2 className='text-bold text-2xl'>Advanced Statistics</h2>
+                <p>Track how your links are performing across the web and with our advance Statistics board</p>
             </div>
             {/* advance cards */}
             <div>
