0418cd72417c921aee1f320244749c3a4759773c
Alice2212 <hezealice@gmail.com>
2023-01-13 13:44:55 +0100

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,16 @@ import heroImg from "../assets/images/illustration-working.svg";
 const Hero = () => {
   return (
     <>
-      <section className="max-width flex ">
+      <section className="max-width flex lg:flex-nowrap md:flex-wrap flex-row my-12">
         <div className="">
-          <h1>MORE THAN JUST SHORTHER LINKS</h1>
+          <h1 className="">MORE THAN JUST SHORTHER LINKS</h1>
           <p>
             Build your brand's recognition and get detailed insights on how your
             links are performing
           </p>
           <button className="btn-cta">Get Started</button>
         </div>
-        <div>
+        <div className="">
           <img src={heroImg} alt="hero iamge"/>
         </div>
       </section>
