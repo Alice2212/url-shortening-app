fad67ec75740ef57f8bee368c0bff6fdd56fc16c
Alice2212 <hezealice@gmail.com>
2023-02-17 12:41:49 +0100

    advance section

--- src/components/Advance.jsx
+++ src/components/Advance.jsx
@@ -9,7 +9,13 @@ const Advance = () => {
       <section className="advance-wrapper bg-slate-200">
         <div className="max-width py-16">
           {/* advance title */}
-
+          <div className="text-center">
+            <h2 className="text-bold text-2xl">Advanced Statistics</h2>
+            <p>
+              Track how your links are performing across the web and with our
+              advance Statistics board
+            </p>
+          </div>
           {/* advance cards */}
           <div></div>
         </div>
