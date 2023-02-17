4e5a7025b698c0aa8ba82b406f71510ecdcf2d7f
Alice2212 <hezealice@gmail.com>
2023-02-17 12:41:21 +0100

    advance section

--- src/components/Advance.jsx
+++ src/components/Advance.jsx
@@ -1,4 +1,4 @@
-import React from 'react' 
+import React from "react";
 import brandImg from "../assets/images/icon-brand-recognition.svg";
 import detailImg from "../assets/images/icon-detailed-records.svg";
 import customizableImg from "../assets/images/icon-fully-customizable.svg";
@@ -6,23 +6,16 @@ import customizableImg from "../assets/images/icon-fully-customizable.svg";
 const Advance = () => {
   return (
     <>
-      <section className='advance-wrapper bg-slate-200'>
-        <div className='max-width py-16'>
-            {/* advance title */}
-            <div className='text-center'>
-                <h2 className='text-bold text-2xl'>Advanced Statistics</h2>
-                <p>Track how your links are performing across the web and with our advance Statistics board</p>
-            </div>
-            {/* advance cards */}
-            <div>
+      <section className="advance-wrapper bg-slate-200">
+        <div className="max-width py-16">
+          {/* advance title */}
 
-            </div>
-
+          {/* advance cards */}
+          <div></div>
         </div>
-
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Advance
+export default Advance;
