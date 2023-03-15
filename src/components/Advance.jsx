5f705e0295ec85b30cc66e87109e09a58cdaffdc
Alice2212 <hezealice@gmail.com>
2023-03-15 11:36:27 +0100

    advance detail added

--- src/components/Advance.jsx
+++ src/components/Advance.jsx
@@ -12,12 +12,12 @@ const Advance = () => {
           <div className="text-center">
             <h2 className="text-bold text-3xl">Advanced Statistics</h2>
             <p className=" text-slate-400 mb-10 lg:mb-20 ">
-              Track how your links are performing across the web and with <br/> our
-              advance Statistics board 
+              Track how your links are performing across the web and with <br />{" "}
+              our advance Statistics board
             </p>
           </div>
           {/* advance cards */}
-          <div className="grid">
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
             {/* card one */}
             <div className="bg-white p-5 rounded">
               <img
@@ -34,39 +34,39 @@ const Advance = () => {
                 your content
               </p>
             </div>
-          </div>
 
-          {/* card two */}
-          <div className="bg-white p-5 rounded">
-            <img
-              src={detailImg}
-              alt="detail image"
-              className="bg-indigo-900 p-2 rounded-full"
-            />
-            <h3 className="text-slate-800 text-bold mb-3 font-bold">
-              Detailed Record
-            </h3>
-            <p className="text-slate-400">
-              Gain insight into who is clicking your links. knowing when and
-              where people engage with your content helps inform better
-              decisions.
-            </p>
-          </div>
+            {/* card two */}
+            <div className="bg-white p-5 rounded">
+              <img
+                src={detailImg}
+                alt="detail image"
+                className="bg-indigo-900 p-2 rounded-full"
+              />
+              <h3 className="text-slate-800 text-bold mb-3 font-bold">
+                Detailed Record
+              </h3>
+              <p className="text-slate-400">
+                Gain insight into who is clicking your links. knowing when and
+                where people engage with your content helps inform better
+                decisions.
+              </p>
+            </div>
 
-          {/* card three */}
-          <div className="bg-white p-5 rounded">
-            <img
-              src={customizableImg}
-              alt="customizable image"
-              className="bg-indigo-900 p-2 rounded-full"
-            />
-            <h3 className="text-slate-800 text-bold mb-3 font-bold">
-              Fully customizable
-            </h3>
-            <p className="text-slate-400">
-              Improve rand awareness and content discoverability through
-              customizable links, superchanging audience engagement{" "}
-            </p>
+            {/* card three */}
+            <div className="bg-white p-5 rounded">
+              <img
+                src={customizableImg}
+                alt="customizable image"
+                className="bg-indigo-900 p-2 rounded-full"
+              />
+              <h3 className="text-slate-800 text-bold mb-3 font-bold">
+                Fully customizable
+              </h3>
+              <p className="text-slate-400">
+                Improve rand awareness and content discoverability through
+                customizable links, superchanging audience engagement{" "}
+              </p>
+            </div>
           </div>
         </div>
       </section>
