1f48061206f0e2d59d23d0def89744a543bd8d56
Alice2212 <hezealice@gmail.com>
2023-03-23 20:32:12 +0100

    shortner events

--- src/components/Shortner.jsx
+++ src/components/Shortner.jsx
@@ -5,7 +5,14 @@ import shortnerDesktop from "../assets/images/bg-shorten-desktop.svg";
 // https://api.shrtco.de/v2/shorten?url=
 
 function Shortner() {
-  const [text, setText] = useState("   ");
+  // declear useState
+  const [text, setText] = useState("  ");
+
+  // handle submit
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
     <>
       <section className="max-width shortner relative ">
@@ -15,17 +22,17 @@ function Shortner() {
         </picture>
 
         {/* form */}
-        <form className="form m-auto">
+        <form className="form m-auto" onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row gap-2 m-auto">
             <input
               type="url"
               placeholder="Shorten a link here.."
               className="w-full px-5 py-2 rounded-lg mb-2 md:mb-0"
               value={text}
-              onChange={(e) => setText(e.target.value)} 
+              onChange={(e) => setText(e.target.value)}
             />
             <button className=" bg-[#4ecccb] rounded-md px-4 py-2 text-white font-bold w-full md:w-auto">
-              Shorten It!
+              Shorten It! onClick = {handleSubmit}
             </button>
           </div>
         </form>
